1195f913b2a7c814a46f0e1585e0ace8f1cd1dce
Khankee <1940930@brunel.ac.uk>
2023-01-31 01:47:01 +0000

    Project init

---
+++ src/main/java/brunel/ac/uk/ofsapp/OfsAppApplication.java
@@ -0,0 +1,13 @@
+package brunel.ac.uk.ofsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brunel/ac/uk/ofsapp/OfsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package brunel.ac.uk.ofsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
