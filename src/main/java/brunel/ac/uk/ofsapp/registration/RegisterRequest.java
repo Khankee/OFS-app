e34a1be698f70e2793d1a4e254a6f1091124d6b8
Khankee <1940930@brunel.ac.uk>
2023-02-01 04:21:51 +0000

    Controllers added and tested

---
+++ src/main/java/brunel/ac/uk/ofsapp/registration/RegisterRequest.java
@@ -0,0 +1,13 @@
+package brunel.ac.uk.ofsapp.registration;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
+}
