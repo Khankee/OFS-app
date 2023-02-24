2936b355c4736d67ee1b20c4265b08d7aa446eb1
Khankee <1940930@brunel.ac.uk>
2023-02-24 01:11:02 +0000

    small add

---
+++ src/main/java/brunel/ac/uk/ofsapp/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package brunel.ac.uk.ofsapp.repository;
+
+import brunel.ac.uk.ofsapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.name = 'ROLE_ADMIN'")
+    List<User> findAdminUsers();
+
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.name = 'ROLE_USER'")
+    List<User> findAllUsersWithUserRole();
+}
